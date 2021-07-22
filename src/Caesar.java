c3bd06a62571b69a1f161dafa6756f3a15676b78
etcodr <ethan@iesdisplays.com>
2021-07-22 16:04:09 -0400

    added Caesar.java file

---
+++ src/Caesar.java
@@ -0,0 +1,30 @@
+/**
+     * TODO:
+     *  [] ensure there are no more than one CLI args
+     *  [] error check the provided argument thusly
+     *    [] loop through input string
+     *    [] check if char in CLI is a digit, if so jump back to start of loop
+     *    [] return error message if CLI is not a digit and gracefully quit
+     *  [] receive the plaintext message to be encrypted from the user
+     *  [] write a function to perform the Caesar shift
+     *    [] create main loop of user inputed message
+     *      [] check if char in message is alphabetical
+     *        [] if so, check if char is uppercase
+     *          [] convert ASCII to alphabetical index
+     *          [] shift alphabetical index using formula
+     *          [] convert back to ASCII
+     *          [] print resulting char
+     *        [] check if char in message is lowercase
+     *          [] convert ASCII to alphabetical index
+     *          [] shift alphabetical index using formula
+     *          [] convert back to ASCII
+     *          [] print resulting char
+     *      [] else print char as is
+     *    
+     */
+
+public class Caesar {
+  public static void main(String[] args) {
+    
+  }
+}
